94d6029a054107f547b30004c551e6dec684b0db
すー <aaaiiiuuu35@gmail.com>
2019-08-23 23:35:03 +0900

    Update UserField.tsx

--- components/UserField.tsx
+++ components/UserField.tsx
@@ -1,10 +1,18 @@
 import * as React from "react";
 import styled from 'styled-components';
+import {StyleSheet, View} from "react-native";
 
-const UserField = styled.div`
-    margin: 2rem 0;
-`;
+// const UserField = styled.div`
+//     margin: 2rem 0;
+// `;
 
-const UserFieldComponent = ({...props}: any) => <UserField {...props} />;
+const styles = StyleSheet.create({
+    userFiled: {
+        marginTop: 2,
+        marginBottom: 2,
+    }
+});
+
+const UserFieldComponent = ({...props}: any) => <View style={styles.userFiled} {...props} />;
 
 export default UserFieldComponent;
