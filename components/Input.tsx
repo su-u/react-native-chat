1ba3853b47863cd83a41f48bb4459385512b545e
すー <aaaiiiuuu35@gmail.com>
2019-08-23 23:35:01 +0900

    Update Input.tsx

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,17 +1,17 @@
 import * as React from "react"
-import styled from 'styled-components';
+import {FormInput} from "react-native";
 
-const Input = styled.input.attrs({
-    type: 'text',
-})`
-    padding: 0.5rem;
-    border: 1px solid #ddd;
-    border-radius: 2px;
-    color: #000000;
-    font-size: 0.8rem;
-    width: 50%;
-`;
+// const Input = styled.input.attrs({
+//     type: 'text',
+// })`
+//     padding: 0.5rem;
+//     border: 1px solid #ddd;
+//     border-radius: 2px;
+//     color: #000000;
+//     font-size: 0.8rem;
+//     width: 50%;
+// `;
 
-const InputComponent = ({...props}: any) => <Input {...props} />;
+const InputComponent = ({...props}: any) => <FormInput {...props} />;
 
 export default InputComponent;
